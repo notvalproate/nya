98be42fadd4605083da018b1f767a3f97a9d2878
Ian Tauro <sushruth127@gmail.com>
2024-09-16 11:06:29 +0530

    Implementation of nya 1

--- src/engine.py
+++ src/engine.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import numpy as np
+import os
 
 class ImageData:
     width = int
     height = int
     pixels = np.array
 
-    def init(self, path):
+    def __init__(self, path):
         img = Image.open(path)
         self.width, self.height = img.size
         img = img.convert("RGBA")
@@ -14,4 +15,19 @@ class ImageData:
 
 def convert_to_nya(image_path: str, output_dir: str) -> None:
     image_data = ImageData(image_path)
-    print(image_data.pixels)
+
+
+    file_name = image_path.split(os.sep)[-1].split(".")[0]
+    output_file = f'{output_dir}{os.sep}{file_name}.nya'
+
+    with open(output_file, "wb") as f:
+        width_bytes = image_data.width.to_bytes(16, byteorder="big")
+        height_bytes = image_data.height.to_bytes(16, byteorder="big")
+
+        f.write(width_bytes)
+        f.write(height_bytes)
+
+        pixels_flat = image_data.pixels.flatten()
+        pixel_data = pixels_flat.astype(np.int8).tobytes()
+
+        f.write(pixel_data)
