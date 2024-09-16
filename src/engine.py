9a9d3bc03e39960b877d3c30b1d0261437cf5522
Ian Tauro <sushruth127@gmail.com>
2024-09-16 11:44:32 +0530

    Ready to add the encoder and decoder

--- src/engine.py
+++ src/engine.py
@@ -14,6 +14,16 @@ class ImageData:
         img = img.convert("RGBA")
         self.pixels = np.array(img)
 
+# a compression/encoding algorithm that compresses the data in some way or the other
+def nparray_to_nya_bytes(pixels: np.array, width: int, height: int) -> bytes:
+    pixels_flat = pixels.flatten()
+    pixel_data = pixels_flat.astype(np.uint8).tobytes()
+    return pixel_data
+
+# a decompression/decoding algorithm that decompresses the data in some way or the other
+def nya_bytes_to_nparray(nya_bytes: bytes, width: int, height: int) -> np.array:
+    return np.frombuffer(nya_bytes, dtype=np.uint8).reshape((height, width, 4))
+
 def display_image_pixels(pixels: np.array) -> None:
     plt.imshow(pixels)
     plt.axis('off')
@@ -21,9 +31,6 @@ def display_image_pixels(pixels: np.array) -> None:
     
 def convert_to_nya(image_path: str, output_dir: str) -> None:
     image_data = ImageData(image_path)
-
-    # Display image for testing
-    display_image_pixels(image_data.pixels)
 
     file_name = image_path.split(os.sep)[-1].split(".")[0]
     output_file = f'{output_dir}{os.sep}{file_name}.nya'
@@ -35,8 +42,7 @@ def convert_to_nya(image_path: str, output_dir: str) -> None:
         f.write(width_bytes)
         f.write(height_bytes)
 
-        pixels_flat = image_data.pixels.flatten()
-        pixel_data = pixels_flat.astype(np.uint8).tobytes()
+        pixel_data = nparray_to_nya_bytes(image_data.pixels, image_data.width, image_data.height)
 
         f.write(pixel_data)
 
@@ -46,6 +52,6 @@ def display_nya_file(nya_file: str) -> None:
         height = int.from_bytes(f.read(16), byteorder="big")
 
         pixel_data = f.read()
-        pixels = np.frombuffer(pixel_data, dtype=np.uint8).reshape((height, width, 4))
+        pixels = nya_bytes_to_nparray(pixel_data, width, height)
 
         display_image_pixels(pixels)
