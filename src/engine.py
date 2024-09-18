62b94c081b49d50d9abadd8cccd6b29d8df980a9
Ian Tauro <sushruth127@gmail.com>
2024-09-18 14:39:30 +0530

    Looking okay

--- src/engine.py
+++ src/engine.py
@@ -16,7 +16,7 @@ class NYA_HEADER:
 
 class NYA_BLOCK(ABC):
     @abstractmethod
-    def to_bytes(self) -> bytes:
+    def to_bits(self) -> bitarray:
         pass
 
 
@@ -26,8 +26,10 @@ class NYA_SINGLE(NYA_BLOCK):
     def __init__(self, value: NDArray[np.uint8]):
         self.VALUE = value
 
-    def to_bytes(self) -> bytes:
-        return self.tag.frombytes(self.VALUE.tobytes())
+    def to_bits(self) -> bitarray:
+        bits = self.tag.copy()
+        bits.frombytes(self.VALUE.tobytes())
+        return bits
 
 
 class NYA_RUN(NYA_SINGLE):
@@ -37,9 +39,12 @@ class NYA_RUN(NYA_SINGLE):
         super().__init__(value)
         self.LENGTH = length
 
-    def to_bytes(self) -> bytes:
+    def to_bits(self) -> bitarray:
         length_str = f'{self.LENGTH-1:06b}'
-        return self.tag.frombytes(self.VALUE.tobytes()).extend([int(bit) for bit in length_str])
+        bits = self.tag.copy()
+        bits.frombytes(self.VALUE.tobytes())
+        bits.extend([int(bit) for bit in length_str])
+        return bits
 
 
 class NYA_SINGLE_HUFFMAN(NYA_BLOCK):
@@ -48,8 +53,10 @@ class NYA_SINGLE_HUFFMAN(NYA_BLOCK):
     def __init__(self, code: bitarray):
         self.CODE = code
 
-    def to_bytes(self) -> bytes:
-        return self.tag.extend(self.CODE)
+    def to_bits(self) -> bitarray:
+        bits = self.tag.copy()
+        bits.extend(self.CODE)
+        return bits
 
 
 class NYA_RUN_HUFFMAN(NYA_SINGLE_HUFFMAN):
@@ -59,17 +66,26 @@ class NYA_RUN_HUFFMAN(NYA_SINGLE_HUFFMAN):
         super().__init__(code)
         self.LENGTH = length
 
-    def to_bytes(self) -> bytes:
+    def to_bits(self) -> bitarray:
         length_str = f'{self.LENGTH-1:06b}'
-        return self.tag.extend(self.CODE).extend([int(bit) for bit in length_str])
+        bits = self.tag.copy()
+        bits.extend(self.CODE)
+        bits.extend([int(bit) for bit in length_str])
+        return bits
 
 
 def nparray_to_nya_bytes(pixels: np.array, width: int) -> bytes:
-    # STAGE 0: CREATE EMPTY HEADER WHICH WILL BE MODIFIED AS WE GO 
+
+    ################################################################
+    # STAGE 0: CREATE EMPTY HEADER WHICH WILL BE MODIFIED AS WE GO #
+    ################################################################
+
     header = NYA_HEADER()
     header.WIDTH = width
 
-    # STAGE 1: DECIDE BETWEEN 3 OR 4 CHANNELS
+    ###########################################
+    # STAGE 1: DECIDE BETWEEN 3 OR 4 CHANNELS #
+    ###########################################
 
     for row in pixels:
         for pixel in row:
@@ -83,21 +99,27 @@ def nparray_to_nya_bytes(pixels: np.array, width: int) -> bytes:
         pixels = pixels[:, :, :3]
         previous = np.array([255, 255, 255])
 
-    # STAGE 2: APPLY THE DIFFERENCE FILTER
+    ########################################
+    # STAGE 2: APPLY THE DIFFERENCE FILTER #
+    ########################################
 
-    for row in pixels:
-        i = 0
-        while i < len(row):
-            diff = row[i] - previous
-            previous = row[i].copy()
-            row[i] = diff
-            i += 1
+    # for row in pixels:
+    #     i = 0
+    #     while i < len(row):
+    #         diff = row[i] - previous
+    #         previous = row[i].copy()
+    #         row[i] = diff
+    #         i += 1
     
+    #######################################################################################################################
+    # STAGE 3: PERFORM RLE ENCODING AND STORE IT IN nya_pixels. nya_values WILL STORE THE COUNTS OF EACH DIFFERENCE/PIXEL #
+    #######################################################################################################################
+
     # STAGE 3.1: FLATTEN PIXELS AND TREAT IT AS 1D ARRAY
     
     pixels = pixels.reshape(-1, pixels.shape[-1])
 
-    # STAGE 3: PERFORM RLE ENCODING AND STORE IT IN nya_pixels. nya_values WILL STORE THE COUNTS OF EACH DIFFERENCE/PIXEL
+    # STAGE 3.2: PERFORM THE RLE ENCODING
 
     nya_pixels = []
     nya_values = {}
@@ -111,55 +133,43 @@ def nparray_to_nya_bytes(pixels: np.array, width: int) -> bytes:
         while ind + length < pixel_count and length <= 64 and np.array_equal(curr_pixel, pixels[ind + length]):
             length += 1
 
+        if length == 1:
+            nya_pixels.append(NYA_SINGLE(curr_pixel))
+        else:
+            nya_pixels.append(NYA_RUN(curr_pixel, length))
+            ind += length - 1
+
         if tuple(pixels[ind]) in nya_values:
             nya_values[tuple(pixel)] += 1
         else:
             nya_values[tuple(pixel)] = 1
 
+        ind += 1
 
-    # STAGE 4: PERFORM HUFFMAN ENCODING ON THE MOST COMMON 256 DIFFERENCES
+    ########################################################################
+    # STAGE 4: PERFORM HUFFMAN ENCODING ON THE MOST COMMON 256 DIFFERENCES #
+    ########################################################################
 
-    # STAGE 5: WRITE EVERYTHING TO A BITARRAY AND THEN RETURN THE BYTES
+    #####################################################################
+    # STAGE 5: WRITE EVERYTHING TO A BITARRAY AND THEN RETURN THE BYTES #
+    #####################################################################
 
-    pixels_flat = pixels.reshape(-1, pixels.shape[-1])
-    pixels_data = bitarray(endian="big")
+    #############################
+    # STAGE 6: RETURN THE BYTES #
+    #############################
 
-    i = 0
-    previous_pixel = pixels_flat[0]
-    pixel_count = len(pixels_flat)
+    nya_data = bitarray(endian="big")
 
-    while i < pixel_count:
-        count = 0
+    # STAGE 6.1: ADD HEADER
+    nya_data.append(int(header.ALPHA_ENCODING))
+    nya_data.append(int(header.VERTICAL_ENCODING))
+    nya_data.frombytes(header.WIDTH.to_bytes(2, byteorder="big"))
 
-        while count <= 256 and i < pixel_count and np.array_equal(pixels_flat[i], previous_pixel):
-            count += 1
-            i += 1
+    # STAGE 6.2: ADD PIXELS
+    for block in nya_pixels:
+        nya_data.extend(block.to_bits())
 
-        if count == 0:
-            pixels_data.extend([0])
-
-            pixels_data.frombytes(pixels_flat[i].tobytes())
-            previous_pixel = pixels_flat[i]
-        else:
-            pixels_data.extend([1])
-
-            adjusted_count = count - 1
-            count_length = adjusted_count.bit_length()
-            length_length = count_length.bit_length()
-
-            count_str = f'{adjusted_count:0{count_length}b}'
-            length_str = f'{length_length:03b}'
-
-            # Add Length for 4 bits
-            pixels_data.extend([int(bit) for bit in length_str])
-            # Add Count for count_length bits
-            pixels_data.extend([int(bit) for bit in count_str])
-
-            i -+ 1
-            
-        i += 1
-
-    return pixels_data.tobytes()
+    return nya_data.tobytes()
     
 def convert_to_nya(image_path: str, output_dir: str) -> bool:
     img = Image.open(image_path)
@@ -171,14 +181,7 @@ def convert_to_nya(image_path: str, output_dir: str) -> bool:
     output_file = f'{output_dir}{os.sep}{file_name}.nya'
 
     with open(output_file, "wb") as f:
-        width_bytes = width.to_bytes(16, byteorder="big")
-        height_bytes = height.to_bytes(16, byteorder="big")
-
-        f.write(width_bytes)
-        f.write(height_bytes)
-
-        pixel_data = nparray_to_nya_bytes(pixels, width)
-
-        f.write(pixel_data)
+        nya_data = nparray_to_nya_bytes(pixels, width)
+        f.write(nya_data)
 
     return True
